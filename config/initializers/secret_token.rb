729de9915ce9d2cd1a53bea9b5fce4a2c48982ae
Jarno Lonardi <jarno.lonardi@helsinki.fi>
2013-05-17 13:50:48 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Viherkatto::Application.config.secret_token = '7e23ebf0989bb649f8a0992d83cc48196a6daed92cb20546887700f22e363042e25f22eab050b63132c7803d1f396683793c22a3ba5d329bbdbddf1a800b3d50'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Viherkatto::Application.config.session_store :cookie_store, key: '_Viherkatto_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Viherkatto::Application.config.session_store :active_record_store
