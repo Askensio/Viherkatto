a51d4a1b9cb511602f74f07acc5fcbb99fdc8a57
Jarno Lonardi <jarno.lonardi@helsinki.fi>
2013-05-21 17:54:17 +0300

    Rekisteröityminen ja User model valmiita

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20130521130517) do
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "remember_token"
+    t.boolean  "admin",           :default => false
+    t.string   "phone"
+    t.string   "profession"
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
+  end
 
 end
