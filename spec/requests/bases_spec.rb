8b15f31a7ec865764d7d336afbf8fb3ce4e3371c
Emmi Otava <emirir5@gmail.com>
2013-05-23 14:35:06 +0300

    testejä lomakkeelle

--- spec/requests/bases_spec.rb
+++ spec/requests/bases_spec.rb
@@ -13,5 +13,25 @@ describe 'Bases' do
     it "should have the content 'Pohja'" do
       page.should have_content('Pohja')
     end
+
+    describe "with invalid information" do
+      it "should not add the base" do
+        expect { click_button submit }.not_to change(Base, :count)
+      end
+    end
+
+    describe "with valid information" do
+      before do
+        fill_in "Absorbancy", with: 100
+        fill_in "Material", with: "kivimurska"
+        fill_in "Thickness", with: 100
+        fill_in "Weight", with: 100
+        fill_in "Note", with: "Aika hyvä ja kestävä materiaali."
+      end
+      it "should add the base" do
+        expect { click_button submit }.to change(Base, :count).by(1)
+      end
+    end
+
   end
 end
