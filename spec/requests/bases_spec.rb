92eabfa9f16f848b14328490b4ddb09e313b1b3c
Emmi Otava <emirir5@gmail.com>
2013-05-28 16:26:52 +0300

    base testejä

--- spec/requests/bases_spec.rb
+++ spec/requests/bases_spec.rb
@@ -6,19 +6,30 @@ describe 'Bases' do
 
   subject { page }
 
-  describe "Base page" do
+  describe 'Add base' do
 
-    before { visit add_base_path }
+    let(:admin) { FactoryGirl.create(:admin) }
+
+    before do
+      sign_in admin
+      visit add_base_path
+    end
 
     let(:submit) { "Lisää pohja" }
 
-    it "should have the content 'Pohja'" do
-      page.should have_content('Pohja')
+    it "should have the content 'Lisää pohja'" do
+      page.should have_content('Lisää pohja')
     end
 
     describe "with invalid information" do
       it "should not add the base" do
         expect { click_button submit }.not_to change(Base, :count)
+      end
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector('title', text: 'Lisää pohja') }
+        it { should have_content('virhe') }
       end
     end
 
@@ -35,4 +46,33 @@ describe 'Bases' do
     end
 
   end
+
+  describe 'Edit base' do
+
+    let(:admin) { FactoryGirl.create(:admin) }
+
+    before do
+      sign_in admin
+      @test_base = Base.create(material: "Kivimurska", thickness: 51, absorbancy: 31, weight: 102)
+      visit edit_basis_path(@test_base)
+    end
+
+    describe "can't view update form if not logged in as admin" do
+      before do
+        visit '/uloskirjaus'
+        visit edit_basis_path(@test_base)
+      end
+      it { should have_selector('title', text: 'Etusivu') }
+    end
+
+    describe "admin can update" do
+      before do
+        fill_in "base_material", with: "Hake"
+        click_button "Päivitä muutokset"
+      end
+      it { should have_content('Hake') }
+    end
+
+  end
+
 end
