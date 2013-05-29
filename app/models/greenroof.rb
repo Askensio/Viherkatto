b7cc7a2f8d1b9bcdccaff78a6e2383aa593fb1c6
Emmi Otava <emirir5@gmail.com>
2013-05-29 12:15:42 +0300

    greenroof modelia

--- app/models/greenroof.rb
+++ app/models/greenroof.rb
@@ -1,7 +1,18 @@
 class Greenroof < ActiveRecord::Base
+
   belongs_to :user
+
   has_many :plants
   has_many :roofs
   has_many :bases
-  attr_accessible :plants, :roofs, :bases, :address, :purpose, :note
+
+  accepts_nested_attributes_for :plants, :roofs, :bases
+
+  attr_accessible :plants, :roofs, :bases, :address, :purpose, :note, :user_id
+
+  validates :address, presence: true, length: { maximum: 150 }
+  validates :purpose, allow_blank: false, numericality: true, inclusion: {in: (0...2)}, only_integer: true
+  validates :note, length: { maximum: 1500 }
+  validates :user_id, allow_blank: false, numericality: true
+
 end
--- spec/models/greenroof_spec.rb
+++ spec/models/greenroof_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Greenroof do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @groof = Greenroof.create()
+  end
 end
