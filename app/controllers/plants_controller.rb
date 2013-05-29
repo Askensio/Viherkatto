a891d0ffb6d7a6ff28d9847154cda6bfb3df6181
Tomi Virtanen <cobrelli@gmail.com>
2013-05-29 10:46:57 +0300

    vertailuja

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -2,7 +2,7 @@
 
 class PlantsController < ApplicationController
 
-  before_filter :admin_user, only: [:new,:create,:update,:destroy, :edit]
+  before_filter :admin_user, only: [:new, :create, :update, :destroy, :edit]
 
   #respond_to :html, :xml, :json
 
@@ -11,21 +11,73 @@ class PlantsController < ApplicationController
   end
 
   def index
-    @plants = Plant.paginate(page: params[:page])
+    #@plants = Plant.paginate(page: params[:page])
     respond_to do |format|
       @plants = Plant.paginate(page: params[:page])
-      format.html { render :html => @plants}  # index.html.erb
+      format.html { render :html => @plants } # index.html.erb
+
       if params[:page].present?
         @jsonPlants = Plant.paginate(page: params[:page], per_page: params[:per_page])
       else
         @jsonPlants = Plant.all
       end
 
-      #if :per_page.present?
-      format.json  { render :json => {count: @plants.total_entries, plants: @jsonPlants } }
-      #else
-      #format.json { render :json => {count: @plants} }
+      if params[:name].present?
+        #@jsonPlants = nil
+        #@jsonPlants = Plant.first
+        @jsonPlantsDub = [:Plant]
+
+        @jsonPlants.each do |p|
+          if !p.name.downcase.include?(params[:name].downcase)
+            @jsonPlantsDub << p
+          end
+        end
+        @jsonPlants -= @jsonPlantsDub
+      end
+
+      if params[:maintenance].present?
+        @jsonPlantsDub = nil
+        @jsonPlantsDub = [:Plant]
+
+        @jsonPlants.each do |p|
+          if p.maintenance.to_i != params[:maintenance].to_i
+            @jsonPlantsDub << p
+          end
+        end
+        @jsonPlants -= @jsonPlantsDub
+      end
+
+
+      #if params[:maintenance].present?
+      #  @jsonPlantsDub = nil
+      #  @jsonPlantsDub = [:Plant]
+      ##
+      #  @jsonPlants.each do |p|
+      #
+      #    if p.maintenance == params[:maintenance]
+      #      @jsonPlantsDub << p
+      #    end
+      #  end
+      #  @jsonPlants = @jsonPlantsDub
       #end
+
+      #if params[:coverage].present?
+      #  @jsonPlantsDub = nil
+      #  @jsonPlantsDub = [:Plant]
+      #
+      #  @jsonPlants.each do |p|
+      #    if p.coverage == params[:coverage]
+      #      @jsonPlantsDub << p
+      #    end
+      #  end
+      #  @jsonPlants = @jsonPlantsDub
+      #end
+
+
+      #:coverage, :latin_name, :aestethic_appeal, :colour, :light_requirement, :maintenance, :min_soil_thickness, :name, :note, :weight
+
+      format.json { render :json => {count: @plants.total_entries, plants: @jsonPlants} }
+
     end
   end
 
