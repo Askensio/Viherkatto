f1ff460d405c30b3cfc8990025b40db6f12280b1
Emmi Otava <emirir5@gmail.com>
2013-05-29 10:18:51 +0300

    greenroof db migration

--- db/migrate/20130529071441_create_greenroofs.rb
+++ db/migrate/20130529071441_create_greenroofs.rb
@@ -1,13 +1,10 @@
 class CreateGreenroofs < ActiveRecord::Migration
   def change
     create_table :greenroofs do |t|
-      t.belongs_to :user
-      t.has_many :plants
-      t.has_many :roofs
-      t.has_many :bases
       t.string :address
       t.integer :purpose
       t.string :note
+      t.integer :user_id
 
       t.timestamps
     end
