612ceb7431ba19c0f7d3164dfd2af2f01a3dd0c2
Tomi Virtanen <cobrelli@gmail.com>
2013-05-29 11:08:55 +0300

    parametrivertailuja

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -22,59 +22,92 @@ class PlantsController < ApplicationController
         @jsonPlants = Plant.all
       end
 
+      @jsonPlantsDub = [:Plant]
+
       if params[:name].present?
-        #@jsonPlants = nil
-        #@jsonPlants = Plant.first
-        @jsonPlantsDub = [:Plant]
+        #@jsonPlantsDub = [:Plant]
 
         @jsonPlants.each do |p|
           if !p.name.downcase.include?(params[:name].downcase)
             @jsonPlantsDub << p
           end
         end
-        @jsonPlants -= @jsonPlantsDub
+        #@jsonPlants -= @jsonPlantsDub
+      end
+
+      if params[:latin_name].present?
+        @jsonPlants.each do |p|
+          if !p.latin_name.downcase.include?(params[:latin_name].downcase)
+            @jsonPlantsDub << p
+          end
+        end
+        #@jsonPlants -= @jsonPlantsDub
+      end
+
+      if params[:colour].present?
+        @jsonPlants.each do |p|
+          if !p.colour.downcase.include?(params[:colour].downcase)
+            @jsonPlantsDub << p
+          end
+        end
+        #@jsonPlants -= @jsonPlantsDub
       end
 
       if params[:maintenance].present?
-        @jsonPlantsDub = nil
-        @jsonPlantsDub = [:Plant]
-
         @jsonPlants.each do |p|
           if p.maintenance.to_i != params[:maintenance].to_i
             @jsonPlantsDub << p
           end
         end
-        @jsonPlants -= @jsonPlantsDub
+        #@jsonPlants -= @jsonPlantsDub
       end
 
+      if params[:coverage].present?
+        @jsonPlants.each do |p|
+          if p.coverage.to_i != params[:coverage].to_i
+            @jsonPlantsDub << p
+          end
+        end
+        #@jsonPlants -= @jsonPlantsDub
+      end
 
-      #if params[:maintenance].present?
-      #  @jsonPlantsDub = nil
-      #  @jsonPlantsDub = [:Plant]
-      ##
-      #  @jsonPlants.each do |p|
-      #
-      #    if p.maintenance == params[:maintenance]
-      #      @jsonPlantsDub << p
-      #    end
-      #  end
-      #  @jsonPlants = @jsonPlantsDub
-      #end
+      if params[:min_soil_thickness].present?
+        @jsonPlants.each do |p|
+          if p.min_soil_thickness.to_i < params[:min_soil_thickness].to_i
+            @jsonPlantsDub << p
+          end
+        end
+        #@jsonPlants -= @jsonPlantsDub
+      end
 
-      #if params[:coverage].present?
-      #  @jsonPlantsDub = nil
-      #  @jsonPlantsDub = [:Plant]
-      #
-      #  @jsonPlants.each do |p|
-      #    if p.coverage == params[:coverage]
-      #      @jsonPlantsDub << p
-      #    end
-      #  end
-      #  @jsonPlants = @jsonPlantsDub
-      #end
+      if params[:weight_is].present?
+        @jsonPlants.each do |p|
+          if p.weight.to_i != params[:weight_is].to_i
+            @jsonPlantsDub << p
+          end
+        end
+        #@jsonPlants -= @jsonPlantsDub
+      end
 
+      if params[:weight_atleast].present?
+        @jsonPlants.each do |p|
+          if p.weight.to_i < params[:weight_atleast].to_i
+            @jsonPlantsDub << p
+          end
+        end
+        #@jsonPlants -= @jsonPlantsDub
+      end
 
-      #:coverage, :latin_name, :aestethic_appeal, :colour, :light_requirement, :maintenance, :min_soil_thickness, :name, :note, :weight
+      if params[:weight_max].present?
+        @jsonPlants.each do |p|
+          if p.weight.to_i > params[:weight_max].to_i
+            @jsonPlantsDub << p
+          end
+        end
+        #@jsonPlants -= @jsonPlantsDub
+      end
+
+      @jsonPlants -= @jsonPlantsDub
 
       format.json { render :json => {count: @plants.total_entries, plants: @jsonPlants} }
 
