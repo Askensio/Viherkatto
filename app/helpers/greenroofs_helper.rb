ccfc232e0a955155cf8b544681e63602b644c708
laursuom <laur1.suomalainen@gmail.com>
2013-06-03 12:33:12 +0300

    Vaiheessa vielä show, mutta toimii

--- app/helpers/greenroofs_helper.rb
+++ app/helpers/greenroofs_helper.rb
@@ -1,2 +1,23 @@
 module GreenroofsHelper
+
+  def printPlants (greenroof)
+    plants = []
+    output = '';
+    output.html_safe
+    greenroof.plants.each { |plant| plants.push((link_to plant.name, plant_path(plant)).html_safe) }
+    hash = Hash[plants.map.with_index.to_a]
+
+    plants.each_with_index { |plant|
+      if hash[plant] < plants.size - 1
+       output << plant
+       output << ', '
+
+      else
+       output << plant
+
+      end
+    }
+     output.html_safe
+  end
+
 end
