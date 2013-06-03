773e8a00a4e56a9e09e7cf3981395c07294f101b
laursuom <laur1.suomalainen@gmail.com>
2013-06-03 16:09:04 +0300

    Viherkaton show kuvien thumbnaileja ja testejä vaille valmis

--- app/helpers/greenroofs_helper.rb
+++ app/helpers/greenroofs_helper.rb
@@ -9,15 +9,61 @@ module GreenroofsHelper
 
     plants.each_with_index { |plant|
       if hash[plant] < plants.size - 1
-       output << plant
-       output << ', '
+        output << plant
+        output << ', '
 
       else
-       output << plant
+        output << plant
 
       end
     }
-     output.html_safe
+    output.html_safe
   end
 
+  def printBases (greenroof)
+    bases = []
+    output = '';
+    output.html_safe
+    index = 1;
+    greenroof.bases.each_with_index { |base| bases.push("Pohja " << index.to_s << ": " << printLayers(base) << "\n")
+    index.next
+    }
+    hash = Hash[bases.map.with_index.to_a]
+
+
+    bases.each_with_index { |plant|
+      if hash[plant] < bases.size - 1
+        output << plant
+        output << ', '
+
+      else
+        output << plant
+
+      end
+    }
+    output.html_safe
+  end
+
+
+  private
+
+  def printLayers(base)
+    layerpile = []
+    output = '';
+    output.html_safe
+    base.layers.each { |layer| layerpile.push((link_to layer.name, layer_path(layer)).html_safe) }
+    hash = Hash[layerpile.map.with_index.to_a]
+
+    layerpile.each_with_index { |plant|
+      if hash[plant] < layerpile.size - 1
+        output << plant
+        output << ', '
+
+      else
+        output << plant
+
+      end
+    }
+    output.html_safe
+  end
 end
