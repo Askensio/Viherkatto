d83cbdc29c8aa581784b21702189cabe3f667aec
Emmi Otava <emirir5@gmail.com>
2013-06-05 16:05:09 +0300

    paginate kuntoon

--- app/assets/javascripts/pagination/greenroofs.js
+++ app/assets/javascripts/pagination/greenroofs.js
@@ -1,3 +1,14 @@
-var addElement = function (entry) {
-    console.log(entry.user)
+var addElement = function (entry, admin) {
+    var listElement = $('<li></li>');
+    var link = $('<a href=\"/' + this.object + 's/' + entry.id + '\"> Käyttäjän ' + entry.user + ' viherkatto paikassa ' + entry.address + '</a>');
+    listElement.append(link);
+    console.log(admin)
+    if (admin) {
+        listElement.append(' | ');
+        var deleteElement = $('<a href=\"#\" id="/' + this.object + 's/' + entry.id + '\">' + 'poista' + '</a>').click(this.deleteRequest);
+        listElement.append(deleteElement).append(' | ');
+        var editElement = $('<a href=\"/' + this.object + 's/' + entry.id + '/edit\">' + 'muokkaa' + '</a>');
+        listElement.append(editElement);
+    }
+    $('.' + this.object + '-list').append(listElement);
 }
--- app/assets/javascripts/pagination/plants.js
+++ app/assets/javascripts/pagination/plants.js
@@ -4,10 +4,11 @@ var addElement = function (entry, admin) {
     listElement.append(link);
     console.log(admin)
     if (admin) {
-    listElement.append(' | ');
-    var deleteElement = $('<a href=\"#\" id="/' + this.object + 's/' + entry.id + '\">' + 'poista' + '</a>').click(this.deleteRequest);
-    listElement.append(deleteElement).append(' | ');
-    var editElement = $('<a href=\"/' + this.object + 's/' + entry.id + '/edit\">' + 'muokkaa' + '</a>');
-    listElement.append(editElement); }
+        listElement.append(' | ');
+        var deleteElement = $('<a href=\"#\" id="/' + this.object + 's/' + entry.id + '\">' + 'poista' + '</a>').click(this.deleteRequest);
+        listElement.append(deleteElement).append(' | ');
+        var editElement = $('<a href=\"/' + this.object + 's/' + entry.id + '/edit\">' + 'muokkaa' + '</a>');
+        listElement.append(editElement);
+    }
     $('.' + this.object + '-list').append(listElement);
 }
