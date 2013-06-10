2a8ca5fb734c63b178ddd883d78ce1cf6dc923ee
Jarno Lonardi <jarno.lonardi@helsinki.fi>
2013-06-10 15:55:05 +0300

    viherkattojen selaus melkein valmis

--- app/assets/javascripts/pagination/greenroofs.js
+++ app/assets/javascripts/pagination/greenroofs.js
@@ -1,20 +1,64 @@
 $(document).ready(function () {
-    var paginator = new Pagination("greenroofs" ,1, 20)
+    var paginator = new Pagination("greenroofs", 1, 20)
 
     paginator.getObjects(true)
+
+    $('#modal-cancel').click(function (event) {
+        $('#confirm-modal').modal('hide')
+    })
+
+    $('#modal-accept').click(function (event) {
+        $('#confirm-modal').modal('hide')
+    })
+
+    $('#modal-accept').click(paginator.deleteRequest(paginator))
+
 });
 
+var setAcceptButtonId = (function (id) {
+    return function (event) {
+        console.log(id)
+        $('#modal-accept').attr('id', id)
+        $('#confirm-modal').modal('show')
+    }
+})
 
 var addElement = function (entry, admin) {
-    var listElement = $('<li></li>');
-    var link = $('<a href=\"/' + this.object + 's/' + entry.id + '\"> Käyttäjän ' + entry.user + ' viherkatto paikassa ' + entry.address + '</a>');
-    listElement.append(link);
+    var mainElement = $('<div></div>').attr('class', 'hero-unit greenroof-list-hero-unit span4');
+
+    var thumbnailElement = $('<div></div>').attr('class', 'thumbnail pull-left')
+    var thumbnail = $('<img />').attr('src', 'assets/ei_kuvaa_placeholder.gif')
+    thumbnailElement.append(thumbnail)
+    mainElement.append(thumbnailElement)
+
+    var infoElement = $('<div></div>')
+    var ownerHeading = $('<h5>Omistaja:</h5>')
+    var owner = $('<p></p>').append(entry.user)
+    infoElement.append(ownerHeading).append(owner)
+    var addressHeading = $('<h5>Sijainti:</h5>')
+    var address = $('<p></p>').append(entry.address)
+    infoElement.append(addressHeading).append(address)
+
     if (admin) {
-        listElement.append(' | ');
-        var deleteElement = $('<a href=\"#\" id="/' + this.object + 's/' + entry.id + '\">' + 'poista' + '</a>').click(this.deleteRequest);
-        listElement.append(deleteElement).append(' | ');
-        var editElement = $('<a href=\"/' + this.object + 's/' + entry.id + '/edit\">' + 'muokkaa' + '</a>');
-        listElement.append(editElement);
+        var deleteElement = $('<a href=\"#\"\"></a>')
+        deleteElement.attr('class', 'pull-right')
+        //infoElement.append(deleteElement)
+
+        //var removeElement = $('<i></i>').attr('class', 'icon-remove-circle')
+        var removeElement = $('<img />').attr({
+            src: 'assets/close-icon.png',
+            class: 'close-icon',
+            "data-toggle": 'modal',
+            id: entry.id
+        })
+
+        // Shows confirmation dialoguen when clicking the cross in the corner
+        removeElement.click(setAcceptButtonId(entry.id))
+
+        //deleteElement.click(this.deleteRequest(this));
+        deleteElement.append(removeElement)
+        mainElement.append(deleteElement)
     }
-    $('.' + this.object + '-list').append(listElement);
+    mainElement.append(infoElement)
+    $('#hero-div').append(mainElement);
 }
