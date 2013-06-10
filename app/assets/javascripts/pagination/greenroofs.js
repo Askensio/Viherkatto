3f23d7e4ded5c3029dd44f082a6e043e0d01e551
Jarno Lonardi <jarno.lonardi@helsinki.fi>
2013-06-10 23:11:14 +0300

    viherkattojen selaus valmis ja ilme kevennetty

--- app/assets/javascripts/pagination/greenroofs.js
+++ app/assets/javascripts/pagination/greenroofs.js
@@ -2,32 +2,40 @@ $(document).ready(function () {
     var paginator = new Pagination("greenroofs", 1, 20)
 
     paginator.getObjects(true)
+    modalctrl = new ModalController()
+    modalctrl.setListeners(paginator)
+
+    $('.container').attr({
+        style: "width: 75%"
+    })
+});
+
+function ModalController() {
+    this.groofId = 0
+}
+
+ModalController.prototype.setListeners = function (paginator) {
 
     $('#modal-cancel').click(function (event) {
         $('#confirm-modal').modal('hide')
     })
 
     $('#modal-accept').click(function (event) {
-        $('#confirm-modal').modal('hide')
+        acceptListenerAction(event)
     })
 
-    $('#modal-accept').click(paginator.deleteRequest(paginator))
-
-});
-
-var setAcceptButtonId = (function (id) {
-    return function (event) {
-        console.log(id)
-        $('#modal-accept').attr('id', id)
-        $('#confirm-modal').modal('show')
+    function acceptListenerAction(event) {
+        console.log(ModalController.groofId)
+        $(event.target).attr('id', 'greenroofs/' + ModalController.groofId)
+        paginator.deleteRequest(paginator)(event)
+        $('#confirm-modal').modal('hide')
     }
-})
+}
 
 var addElement = function (entry, admin) {
     var mainElement = $('<div></div>').attr('class', 'hero-unit greenroof-list-hero-unit span4');
-
     var thumbnailElement = $('<div></div>').attr('class', 'thumbnail pull-left')
-    var thumbnail = $('<img />').attr('src', 'assets/ei_kuvaa_placeholder.gif')
+    var thumbnail = $('<img />').attr('src', '/assets/ei_kuvaa_placeholder.gif')
     thumbnailElement.append(thumbnail)
     mainElement.append(thumbnailElement)
 
@@ -42,23 +50,32 @@ var addElement = function (entry, admin) {
     if (admin) {
         var deleteElement = $('<a href=\"#\"\"></a>')
         deleteElement.attr('class', 'pull-right')
-        //infoElement.append(deleteElement)
-
-        //var removeElement = $('<i></i>').attr('class', 'icon-remove-circle')
         var removeElement = $('<img />').attr({
-            src: 'assets/close-icon.png',
+            src: '/assets/close-icon.png',
             class: 'close-icon',
             "data-toggle": 'modal',
             id: entry.id
         })
 
-        // Shows confirmation dialoguen when clicking the cross in the corner
-        removeElement.click(setAcceptButtonId(entry.id))
+        removeElement.click(function () {
+            $('#confirm-modal').modal('show')
+            ModalController.groofId = entry.id
 
-        //deleteElement.click(this.deleteRequest(this));
+        })
+
         deleteElement.append(removeElement)
         mainElement.append(deleteElement)
+        thumbnailElement.click(function () {
+            window.location.href = "/greenroofs/" + entry.id;
+        })
+        thumbnailElement.attr('style', 'cursor: pointer;')
+    } else {
+        mainElement.click(function () {
+            window.location.href = "/greenroofs/" + entry.id;
+        })
+        mainElement.attr('style', 'cursor: pointer;')
     }
     mainElement.append(infoElement)
+
     $('#hero-div').append(mainElement);
 }
