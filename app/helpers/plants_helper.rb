0eeb4b27fc2226f96af8062e67057be00ae793a1
Tomi Virtanen <cobrelli@gmail.com>
2013-07-01 15:54:17 +0300

    siistimistä ja kommentointia

--- app/helpers/plants_helper.rb
+++ app/helpers/plants_helper.rb
@@ -4,6 +4,7 @@
 
 module PlantsHelper
 
+  # Helper function to list all environment id's
   def listSelectedEnvironments (environments)
     envs = []
     environments.each do |env|
@@ -12,6 +13,7 @@ module PlantsHelper
     return envs
   end
 
+  # Helper function to list all environments
   def printEnvironments (environments)
     envs = ""
     counter = 1
@@ -24,8 +26,4 @@ module PlantsHelper
     end
     return envs
   end
-
-  #def colour_categories
-  #  I18n.t(:colour_categories).map { |key, value| [value, key.to_s] }
-  #end
 end
